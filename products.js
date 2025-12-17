ed78195b5a2f52f645eaee28b75172d4aafce3c1
qahwajidz <jbaghaoui@gmail.com>
2025-12-17 14:42:37 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,6 +1,7 @@
 const products = [
-  { id: 1, name: "Café torréfié – 250 g", price: 1200, category: "coffee", available: true, image: "images/coffee.jpg" },
-  { id: 2, name: "Café torréfié – 1 kg", price: 4200, category: "coffee", available: false, image: "images/coffee.jpg" },
-  { id: 3, name: "Œufs fermiers", price: 600, category: "farm", available: true, image: "images/eggs.jpg" },
-  { id: 4, name: "Confiture maison", price: 800, category: "transform", available: true, image: "images/jam.jpg" }
+  { id: 1, name: "Café torréfié – 250 g", price: 800, category: "coffee", available: true, image: "images/coffee.jpg" },
+  { id: 2, name: "Café torréfié – 1 kg", price: 3000, category: "coffee", available: true, image: "images/coffee.jpg" },
+  { id: 3, name: "Œufs fermiers", price: 600, category: "farm", available: false, image: "images/eggs.jpg" },
+  { id: 4, name: "Confiture maison", price: 800, category: "transform", available: true, image: "images/jam.jpg" },
+  { id: 5, name: "Café torréfié – sampler", price: 2500, category: "coffee", available: true, image: "images/coffee.jpg" }
 ];

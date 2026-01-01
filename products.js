46faf08210cb724d7475cc556143e7b3fb5bad97
qahwajidz <jbaghaoui@gmail.com>
2026-01-01 14:48:37 +0100

    Update products.js
    with description option

--- products.js
+++ products.js
@@ -1,7 +1,129 @@
 const products = [
-  { id: 1, name: "Café torréfié – 250 g", price: 800, category: "coffee", available: true, image: "images/coffee.jpg" },
-  { id: 2, name: "Café torréfié – 1 kg", price: 3000, category: "coffee", available: true, image: "images/coffee.jpg" },
-  { id: 3, name: "Œufs fermiers", price: 600, category: "farm", available: false, image: "images/eggs.jpg" },
-  { id: 4, name: "Confiture maison", price: 800, category: "transform", available: true, image: "images/jam.jpg" },
-  { id: 5, name: "Café torréfié – sampler", price: 2500, category: "coffee", available: true, image: "images/coffee.jpg" }
+  {
+    id: 1,
+    name: "Café torréfié – 250 g",
+    description: "100% Arabica, Ethiopie ou Brésil",
+    price: 800,
+    category: "coffee",
+    available: true,
+    image: "images/coffee.jpg"
+  },
+  {
+    id: 2,
+    name: "Café torréfié – 1 kg",
+    description: "100% Arabica, Ethiopie ou Brésil",
+    price: 3000,
+    category: "coffee",
+    available: true,
+    image: "images/coffee.jpg"
+  },
+  {
+    id: 3,
+    name: "Sampler - 3x250g",
+    description: "100% Arabica, Ethiopie ou Brésil, ",
+    price: 2500,
+    category: "coffee",
+    available: true,
+    image: "images/coffee.jpg"
+  },
+  {
+    id: 4,
+    name: "Beurre de cacahuète maison",
+    description: "",
+    price: 800,
+    category: "transform",
+    available: true,
+    image: "images/jam.jpg"
+  },
+  {
+    id: 5,
+    name: "Cactus candy",
+    description: "",
+    price: 3000,
+    category: "transform",
+    available: true,
+    image: "images/jam.jpg"
+  },
+  {
+    id: 6,
+    name: "Œufs fermiers",
+    description: "",
+    price: 600,
+    category: "farm",
+    available: false,
+    image: "images/eggs.jpg"
+  },
+  {
+    id: 7,
+    name: "Orangettes chocolat 70%",
+    description: "",
+    price: 800,
+    category: "transform",
+    available: true,
+    image: "images/jam.jpg"
+  },
+  {
+    id: 8,
+    name: "Chocolate covered espresso beans",
+    description: "",
+    price: 3000,
+    category: "coffee",
+    available: true,
+    image: "images/coffee.jpg"
+  },
+  {
+    id: 9,
+    name: "Feuilles d'olivier façon matcha",
+    description: "",
+    price: 600,
+    category: "farm",
+    available: false,
+    image: "images/eggs.jpg"
+  },
+  {
+    id: 10,
+    name: "Levain naturel",
+    description: "",
+    price: 800,
+    category: "transform",
+    available: true,
+    image: "images/jam.jpg"
+  },
+  {
+    id: 11,
+    name: "Vinaigre de grenade",
+    description: "",
+    price: 3000,
+    category: "transform",
+    available: true,
+    image: "images/coffee.jpg"
+  },
+  {
+    id: 12,
+    name: "Olives",
+    description: "",
+    price: 600,
+    category: "farm",
+    available: false,
+    image: "images/eggs.jpg"
+  },
+  {
+    id: 13,
+    name: "Confiture maison",
+    description: "",
+    price: 800,
+    category: "transform",
+    available: true,
+    image: "images/jam.jpg"
+  },
+  {
+    id: 14,
+    name: "Café torréfié – sampler",
+    description: "",
+    price: 2500,
+    category: "coffee",
+    available: true,
+    image: "images/coffee.jpg"
+  }
 ];
+
